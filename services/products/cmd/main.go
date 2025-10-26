427e6fd6e0edefb2ddec3a329051969dd9b4f024
Seung Hong <74932143+wassup-chicken@users.noreply.github.com>
2025-10-25 23:22:29 -0400

    grpc

--- services/products/cmd/main.go
+++ services/products/cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 
 	"github.com/wassup-chicken/ecomm/services/products/internal/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	api "buf.build/gen/go/wassup-chicken/common/grpc/go/api/v1/apiv1grpc"
 )
 
 func main() {
@@ -12,6 +18,17 @@ func main() {
 
 	ps := server.NewProducts()
 
-	log.Println(ps)
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%v", "8081"))
 
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	reflection.Register(grpcServer)
+
+	api.RegisterProductServiceServer(grpcServer, ps)
+
+	grpcServer.Serve(lis)
 }
